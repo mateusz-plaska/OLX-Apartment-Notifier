b55a9c73a2ed42296995b4d3ddf49b4477f41431
Mateusz Płaska <plaskamateuszi8@gmail.com>
2024-10-07 08:07:26 +0200

    feat: add update user validator

--- olx_apartment_notifier/app/validators/user.ts
+++ olx_apartment_notifier/app/validators/user.ts
@@ -7,3 +7,16 @@ export const createUserValidator = vine.compile(
         password: vine.string().minLength(8)
     })
 )
+
+export const updateUserValidator = (userId: number) => {
+    return vine.compile(
+        vine.object({
+            fullName: vine.string().maxLength(255),
+            email: vine.string().email()
+                .unique(async (db, value) => !(await db.from('users').where('email', value).whereNot('id', userId).first())),
+            password: vine.string().minLength(8).optional()
+        })
+    )
+}
+
+
