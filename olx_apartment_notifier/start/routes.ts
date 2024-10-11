f60778ef6806f1e88e615df6699e923db2a7e29f
Mateusz Płaska <plaskamateuszi8@gmail.com>
2024-10-11 18:20:54 +0200

    refactor: refactor routes file, make groups, add validations and auth

--- olx_apartment_notifier/start/routes.ts
+++ olx_apartment_notifier/start/routes.ts
@@ -11,6 +11,7 @@ import AuthLoginController from '#controllers/auth_login_controller'
 import UserPreferencesController from '#controllers/user_preferences_controller'
 import UsersController from '#controllers/users_controller'
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 
 router.get('', [AuthLoginController, 'getInitialView'])
@@ -21,22 +22,28 @@ router.post('/login', [AuthLoginController, 'login'])
 router.get('/registration', [UsersController, 'getRegistrationView'])
 router.post('/registration', [UsersController, 'create'])
 
-router.get('/home', [UserPreferencesController, 'getHomeViewAndUserPreferences'])
-router.get('/home/profile', [UsersController, 'showUser'])
+router.group(() => {
+    router.get('', [UserPreferencesController, 'getHomeViewAndUserPreferences'])
 
-router.post('/home/profile', [UsersController, 'update'])
-router.get('/home/profile/delete', [UsersController, 'destroy'])
-router.get('/home/profile/logout', [AuthLoginController, 'logout'])
+    router.group(() => {
+        router.get('', [UsersController, 'showUser'])
+        router.post('', [UsersController, 'update'])
+        router.get('/delete', [UsersController, 'destroy'])
+        router.get('/logout', [AuthLoginController, 'logout'])
+    }).prefix('/profile')
+    
+    router.get('/create-preference', [UserPreferencesController, 'getCreatePreferenceView'])
+    router.post('/create-preference', [UserPreferencesController, 'storePreference'])
+    
+    router.get('/update-preference/:userPreferenceId', [UserPreferencesController, 'getUpdatePreferenceView']).where('userPreferenceId', /^[0-9]+$/)
+    router.post('/update-preference/:userPreferenceId', [UserPreferencesController, 'updatePreference']).where('userPreferenceId', /^[0-9]+$/)
 
-router.get('/home/create-preference', [UserPreferencesController, 'getCreatePreferenceView'])
-router.get('/home/update-preference/:userPreferenceId', [UserPreferencesController, 'getUpdatePreferenceView'])
+    router.get('/delete-preference/:userPreferenceId', [UserPreferencesController, 'destroyPreference']).where('userPreferenceId', /^[0-9]+$/)
 
-router.post('/home/create-preference', [UserPreferencesController, 'storePreference'])
-router.post('/home/update-preference/:userPreferenceId', [UserPreferencesController, 'updatePreference'])
-
-router.get('/home/delete-preference/:userPreferenceId', [UserPreferencesController, 'destroyPreference'])
+}).prefix('/home').use(middleware.auth())
 
 
-router.get('/cities-by-region/:regionId', [UserPreferencesController, 'getCitiesByRegion'])
-router.get('/districts-by-city/:cityId', [UserPreferencesController, 'getDistrictsByCity'])
-router.get('/selected-building-types-in-user-preference/:userPreferenceId', [UserPreferencesController, 'getSelectedBuildingTypesInPreference'])
+router.get('/cities-by-region/:regionId', [UserPreferencesController, 'getCitiesByRegion']).where('regionId', /^[0-9]+$/)
+router.get('/districts-by-city/:cityId', [UserPreferencesController, 'getDistrictsByCity']).where('cityId', /^[0-9]+$/)
+router.get('/selected-building-types-in-user-preference/:userPreferenceId', 
+    [UserPreferencesController, 'getSelectedBuildingTypesInPreference']).where('userPreferenceId', /^[0-9]+$/)
