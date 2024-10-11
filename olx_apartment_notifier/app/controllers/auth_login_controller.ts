fd34dcfc0778ec5e5ab8b9cd8bba12c2bd23da5d
Mateusz Płaska <plaskamateuszi8@gmail.com>
2024-10-11 16:43:45 +0200

    feat: create user_preference's controller and add routes

--- olx_apartment_notifier/app/controllers/auth_login_controller.ts
+++ olx_apartment_notifier/app/controllers/auth_login_controller.ts
@@ -1,15 +1,12 @@
 import User from '#models/user'
-import UserPreference from '#models/user_preference'
-import { createUserValidator, updateUserValidator } from '#validators/user'
-import auth from '@adonisjs/auth/services/main'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class AuthLoginController {
-    async showInitialView({view}: HttpContext) {
+    async getInitialView({view}: HttpContext) {
         return view.render('initial_view')
     }
 
-    async showLoginView({view}: HttpContext) {
+    async getLoginView({view}: HttpContext) {
         return view.render('login_view')
     }
 
@@ -26,53 +23,8 @@ export default class AuthLoginController {
         }
     }
 
-    async showRegistrationView({view}: HttpContext) {
-        return view.render('registration_view')
-    }
-
-    async register({request, auth, response}: HttpContext) {
-        const inputData = await createUserValidator.validate(request.all())
-        const user = await User.create(inputData)
-        await auth.use().login(user)
-        return response.redirect('/home')   
-    }
-
-
-    async updateUserData({auth, request, response}: HttpContext) {
-        const user = await auth.use().authenticate()
-        const inputData = await updateUserValidator(user.id).validate(request.all())
-        
-        user.merge(inputData).save()
-        return response.redirect().back()
-    }
-
-    async deleteUser({auth, response}: HttpContext) {
-        const user = await auth.use().authenticate()
-        await user.delete()
-        return response.redirect('/')
-    }
-
     async logout({auth, response}: HttpContext) {
         await auth.use().logout()
         return response.redirect('/')
-    }
-
-    //////////////////////////
-
-
-    async showHomeView({auth, request, view}: HttpContext) {
-        const user = await auth.use().authenticate()
-
-        const userPreferences = await UserPreference.query().where('user_id', user.id).paginate(
-            request.input('page', 1),
-            request.input('perPage', 5)
-        )
-
-        return view.render('home_view', {userPreferences})
-    }
-
-    async showUserProfileView({auth, view}: HttpContext) {
-        const user = await auth.use().authenticate()
-        return view.render('user_profile_view', {user})
     }
 }
--- olx_apartment_notifier/start/routes.ts
+++ olx_apartment_notifier/start/routes.ts
@@ -8,6 +8,7 @@
 */
 
 import AuthLoginController from '#controllers/auth_login_controller'
+import UserPreferencesController from '#controllers/user_preferences_controller'
 import UsersController from '#controllers/users_controller'
 import router from '@adonisjs/core/services/router'
 
@@ -20,9 +21,22 @@ router.post('/login', [AuthLoginController, 'login'])
 router.get('/registration', [UsersController, 'getRegistrationView'])
 router.post('/registration', [UsersController, 'create'])
 
-router.get('/home', [AuthLoginController, 'showHomeView'])
+router.get('/home', [UserPreferencesController, 'getHomeViewAndUserPreferences'])
 router.get('/home/profile', [UsersController, 'showUser'])
 
 router.post('/home/profile', [UsersController, 'update'])
 router.get('/home/profile/delete', [UsersController, 'destroy'])
 router.get('/home/profile/logout', [AuthLoginController, 'logout'])
+
+router.get('/home/create-preference', [UserPreferencesController, 'getCreatePreferenceView'])
+router.get('/home/update-preference/:userPreferenceId', [UserPreferencesController, 'getUpdatePreferenceView'])
+
+router.post('/home/create-preference', [UserPreferencesController, 'storePreference'])
+router.post('/home/update-preference/:userPreferenceId', [UserPreferencesController, 'updatePreference'])
+
+router.get('/home/delete-preference/:userPreferenceId', [UserPreferencesController, 'destroyPreference'])
+
+
+router.get('/cities-by-region/:regionId', [UserPreferencesController, 'getCitiesByRegion'])
+router.get('/districts-by-city/:cityId', [UserPreferencesController, 'getDistrictsByCity'])
+router.get('/selected-building-types-in-user-preference/:userPreferenceId', [UserPreferencesController, 'getSelectedBuildingTypesInPreference'])
