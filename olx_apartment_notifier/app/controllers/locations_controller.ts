f596ad7f72483429bf9644aab8b5417f776cb3cb
Mateusz Płaska <plaskamateuszi8@gmail.com>
2024-10-14 01:26:44 +0200

    refactor: simplify select buildingTypes from preference

--- olx_apartment_notifier/app/controllers/locations_controller.ts
+++ olx_apartment_notifier/app/controllers/locations_controller.ts
@@ -6,7 +6,7 @@ import District from '#models/district';
 export default class LocationsController {
     async getSelectedBuildingTypesInPreference({params, response}: HttpContext) {
         const userPreference = await UserPreference.findOrFail(params.userPreferenceId)
-        const selectedBuildingTypes = await userPreference.related('buildingTypes').query().pivotColumns(['building_type_id'])
+        const selectedBuildingTypes = await userPreference.related('buildingTypes').query()
         const selectedBuildingTypeIds = selectedBuildingTypes.map((buildingType) => (buildingType.id))
         return response.json(selectedBuildingTypeIds)
     }
